aeaf564be858d27ecef416409a3f6825c4bb5aa0
Abdullah-266327 <105458350+Abdullah-266327@users.noreply.github.com>
2025-01-28 23:06:26 +0500

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
-import streamlit as st
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
